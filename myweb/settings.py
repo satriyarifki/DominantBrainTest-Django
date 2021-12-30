adaecacb21eb33edaac0458e70e6f0be0855c332
satriyarp <rifkisatriya@gmail.com>
2021-12-31 00:36:11 +0700

    add staticfiles dir

--- myweb/settings.py
+++ myweb/settings.py
@@ -132,9 +132,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    WindowsPath.joinpath(BASE_DIR, 'static',)]
+    WindowsPath.joinpath(BASE_DIR, 'static',),
+    os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

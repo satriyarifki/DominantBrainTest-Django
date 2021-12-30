f698fa9d724ba0b9295e56eda34d0b2175fdfe8f
satriyarp <rifkisatriya@gmail.com>
2021-12-31 03:00:45 +0700

    delete django heroku

--- myweb/settings.py
+++ myweb/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path, WindowsPath
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,6 +143,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
